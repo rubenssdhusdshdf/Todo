eca92fd8257b56147945212d35655fada573dc61
Ruben <rubenvegajusto@gmail.com>
2023-07-29 10:58:46 +0100

    first commit and props

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+import './App.css';
+import React from 'react';
+
+const defaultTodos = [
+  { text: 'Cortar cebolla', completed: true },
+  { text: 'Tomar el Curso de Intro a React.js', completed: false },
+  { text: 'Llorar con la Llorona', completed: false },
+  { text: 'LALALALALA', completed: false },
+];
+
+function App() {
+  return (
+    <React.Fragment>
+      <TodoCounter completed={16} total={25} />
+      <TodoSearch />
+
+      <TodoList>
+        {defaultTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
+          />
+        ))}
+      </TodoList>
+      
+      <CreateTodoButton />
+    </React.Fragment>
+  );
+}
+
+export default App;
